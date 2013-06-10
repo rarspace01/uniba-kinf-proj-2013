eaf81759829fba08d74039dd61df44ca93716753
LukeSchwoebel@gmail.com <LukeSchwoebel@gmail.com@e6a66caa-e9e3-d0a2-5608-3df70dc90c32>
2013-06-10 13:59:35 +0000

    added colors to geometries

--- src/de/uniba/wiai/kinf/lehre/ma13/model/OrderedObject.java
+++ src/de/uniba/wiai/kinf/lehre/ma13/model/OrderedObject.java
@@ -1,5 +1,6 @@
 package de.uniba.wiai.kinf.lehre.ma13.model;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -9,20 +10,12 @@ import de.uniba.wiai.kinf.lehre.ma13.model.interfaces.IOrderedObject;
 
 public abstract class OrderedObject implements IOrderedObject {
 
-	private boolean visible_;
 	private Long objectId_;
 	
-	private String _name;
-	
-	@Override
-	public void setVisibility(boolean visible) {
-		visible_ = visible;
-	}
-
-	@Override
-	public boolean isVisible() {
-		return visible_;
-	}
+	private String name_;
+	private Color color_;
+	private boolean visible_;
+	private float opacity_ = 1.0f;
 
 	@Override
 	public void setObjectId(Long objectId) {
@@ -36,12 +29,42 @@ public abstract class OrderedObject implements IOrderedObject {
 	
 	@Override
 	public void setName(String name) {
-		_name = name;
+		name_ = name;
 	}
 	
 	@Override
 	public String getName() {
-		return _name;
+		return name_;
+	}
+	
+	@Override
+	public void setColor(Color color) {
+		color_ = color;
+	}
+	
+	@Override
+	public Color getColor() {
+		return color_;
+	}
+	
+	@Override
+	public void setVisibility(boolean visible) {
+		visible_ = visible;
+	}
+
+	@Override
+	public boolean isVisible() {
+		return visible_;
+	}
+
+	@Override
+	public void setOpacity(float opacity) {
+		opacity_ = opacity;
+	}
+
+	@Override
+	public float getOpacity() {
+		return opacity_;
 	}
 
 	@Override
--- src/de/uniba/wiai/kinf/lehre/ma13/view/drawtemplates/DrawPolygonRaw.java
+++ src/de/uniba/wiai/kinf/lehre/ma13/view/drawtemplates/DrawPolygonRaw.java
@@ -3,7 +3,6 @@ package de.uniba.wiai.kinf.lehre.ma13.view.drawtemplates;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.geom.Point2D;
 
 import de.uniba.wiai.kinf.lehre.ma13.controller.interfaces.IAppDelegate;
 import de.uniba.wiai.kinf.lehre.ma13.model.Polygon;
@@ -20,7 +19,11 @@ public class DrawPolygonRaw extends DrawGeometry {
 	{
 		Polygon polygon = (Polygon)geometry;
 		
-		g.setColor(Color.black);
+		g.setColor(new Color(
+				geometry.getColor().getRed(),
+				geometry.getColor().getGreen(),
+				geometry.getColor().getBlue(),
+				Math.round(255 * geometry.getOpacity())));
 		
 		int i = 1;
 		for(i = 1; i <= polygon.getPoints().size(); i++)
