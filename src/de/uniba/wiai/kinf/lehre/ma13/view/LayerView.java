7aac895491ec4fc38a1624198e79428b7279f517
LukeSchwoebel@gmail.com <LukeSchwoebel@gmail.com@e6a66caa-e9e3-d0a2-5608-3df70dc90c32>
2013-06-11 21:40:25 +0000

    added icons for toolbar buttons, added support for visiblity

--- src/de/uniba/wiai/kinf/lehre/ma13/view/LayerView.java
+++ src/de/uniba/wiai/kinf/lehre/ma13/view/LayerView.java
@@ -1,5 +1,8 @@
 package de.uniba.wiai.kinf.lehre.ma13.view;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 
@@ -12,11 +15,36 @@ public class LayerView extends JList<LayerViewListItem> {
 	private static final long serialVersionUID = 1L;
 	private IAppDelegate appDelegate_;
 	private boolean lockrepaint_ = false;
+	private MouseListener mouseListener_ = null;
 	
 
 	public LayerView(IAppDelegate appDelegate)
 	{
 		appDelegate_ = appDelegate;
+		
+		mouseListener_ = new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) { }
+			@Override
+			public void mousePressed(MouseEvent arg0) { }
+			@Override
+			public void mouseExited(MouseEvent arg0) { }
+			@Override
+			public void mouseEntered(MouseEvent arg0) { }
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(arg0.getX() >= 5 && arg0.getX() <= 55)
+				{
+					LayerViewListItem item = getModel().getElementAt(locationToIndex(arg0.getPoint()));
+					item.getObject().setVisibility(!item.getObject().isVisible());
+					System.out.println("Button clicked: " + getModel().getElementAt(locationToIndex(arg0.getPoint())).getObject().getName());
+					appDelegate_.getWindow().refresh();
+				}
+			}
+		};
+		this.addMouseListener(mouseListener_);
 	}
 	
 	public void repaint()
@@ -32,15 +60,15 @@ public class LayerView extends JList<LayerViewListItem> {
 		for (ILayer layer: appDelegate_.getLayerStore().getAllLayers()) {
 			
 			
-			listModel.addElement(new LayerViewListItem("res/layer.png", layer.getName()));
+			listModel.addElement(new LayerViewListItem(layer));
 			
 			
 			for(IGeometry geometry: layer.getGeometries()) {
-				listModel.addElement(new LayerViewListItem("res/polygon.png", geometry.getName()));
+				listModel.addElement(new LayerViewListItem(geometry));
 			}
 		}
 		
-		listModel.addElement(new LayerViewListItem("res/image.png", "Structure"));
+		listModel.addElement(new LayerViewListItem(appDelegate_.getLayerStore().getBackgroundImage()));
 		
 		setModel(listModel);
 		setCellRenderer(new LayerViewCellRenderer());
