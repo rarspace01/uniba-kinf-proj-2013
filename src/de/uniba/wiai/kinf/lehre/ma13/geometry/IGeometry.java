0c0c04cfcc9ebd8bf2bd497954a11876de88e7bf
LukeSchwoebel@gmail.com <LukeSchwoebel@gmail.com@e6a66caa-e9e3-d0a2-5608-3df70dc90c32>
2013-07-13 14:54:48 +0000

    deleted unused files

--- src/de/uniba/wiai/kinf/lehre/ma13/geometry/IGeometry.java
+++
@@ -1,9 +0,0 @@
-package de.uniba.wiai.kinf.lehre.ma13.geometry;
-
-import java.awt.Graphics;
-
-import de.uniba.wiai.kinf.lehre.ma13.ICanvas;
-
-public interface IGeometry {
-	void draw(ICanvas canvas, Graphics graphics);
-}
