9db41238a0f0c2b42dd39fd6041a72c4f090b595
denishamann@gmail.com <denishamann@gmail.com@e6a66caa-e9e3-d0a2-5608-3df70dc90c32>
2013-07-16 10:04:08 +0000

    *FIX: -null color on polygon

--- src/de/uniba/wiai/kinf/lehre/ma13/data/PPolygons.java
+++ src/de/uniba/wiai/kinf/lehre/ma13/data/PPolygons.java
@@ -12,31 +12,59 @@ import de.uniba.wiai.kinf.lehre.ma13.model.interfaces.ILayer;
 
 public class PPolygons {
 
-	public void saveToDB(List<IGeometry> toBeSaved){
-		
-		PPoint ppoint=new PPoint();
-		
+	public void saveToDB(List<IGeometry> toBeSaved) {
+
+		PPoint ppoint = new PPoint();
+
 		for (int i = 0; i < toBeSaved.size(); i++) {
 
 			try {
-				
-				int isVisible=0;
-				if(toBeSaved.get(i).isVisible()){
-					isVisible=1;
+
+				int isVisible = 0;
+				if (toBeSaved.get(i).isVisible()) {
+					isVisible = 1;
 				}
-				
-				DataManagerSQLiteSingleton.getInstance()
-						.execute(
-								"REPLACE INTO polygon (polygonid, layerid, name, isvisible, color, opacity) VALUES ('"
-										+ toBeSaved.get(i).getObjectId()
-										+ "','" + toBeSaved.get(i).getParent().getObjectId()
-										+ "','" + toBeSaved.get(i).getName()
-										+ "','" + isVisible
-										+ "','"
-										+ toBeSaved.get(i).getColor().getRGB()
-										+ "','"
-										+ toBeSaved.get(i).getOpacity()
-										+ "'); ");
+
+				if (toBeSaved.get(i).getColor() == null) {
+					
+					DataManagerSQLiteSingleton
+					.getInstance()
+					.execute(
+							"REPLACE INTO polygon (polygonid, layerid, name, isvisible, opacity) VALUES ('"
+									+ toBeSaved.get(i).getObjectId()
+									+ "','"
+									+ toBeSaved.get(i).getParent()
+											.getObjectId()
+									+ "','"
+									+ toBeSaved.get(i).getName()
+									+ "','"
+									+ isVisible
+									+ "','"
+									+ toBeSaved.get(i).getOpacity()
+									+ "'); ");
+					
+				} else {
+
+					DataManagerSQLiteSingleton
+							.getInstance()
+							.execute(
+									"REPLACE INTO polygon (polygonid, layerid, name, isvisible, color, opacity) VALUES ('"
+											+ toBeSaved.get(i).getObjectId()
+											+ "','"
+											+ toBeSaved.get(i).getParent()
+													.getObjectId()
+											+ "','"
+											+ toBeSaved.get(i).getName()
+											+ "','"
+											+ isVisible
+											+ "','"
+											+ toBeSaved.get(i).getColor()
+													.getRGB()
+											+ "','"
+											+ toBeSaved.get(i).getOpacity()
+											+ "'); ");
+
+				}
 
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -47,52 +75,57 @@ public class PPolygons {
 			ppoint.saveToDB(toBeSaved.get(i));
 
 		}
-		
+
 	}
-	
-	public List<IGeometry> loadFromDB(ILayer layer){
-		
+
+	public List<IGeometry> loadFromDB(ILayer layer) {
+
 		List<IGeometry> polygonList = new LinkedList<IGeometry>();
-		
-		System.out.println("DEBUG: filename: "+DataManagerSQLiteSingleton.getInstance().getCurrentFilename());
-		
-		DataManagerSQLite dataManager=new DataManagerSQLite(DataManagerSQLiteSingleton.getInstance().getCurrentFilename());
-		
+
+		System.out
+				.println("DEBUG: filename: "
+						+ DataManagerSQLiteSingleton.getInstance()
+								.getCurrentFilename());
+
+		DataManagerSQLite dataManager = new DataManagerSQLite(
+				DataManagerSQLiteSingleton.getInstance().getCurrentFilename());
+
 		try {
-			ResultSet resultSet= 		
-					dataManager.select("SELECT polygonid, layerid, name, isvisible, color, opacity FROM polygon WHERE layerid = '"+layer.getObjectId()+"';");
-			
-			while(resultSet.next()){
-				
-				IGeometry polygon=new Polygon(resultSet.getLong("polygonid"));
-				
+			ResultSet resultSet = dataManager
+					.select("SELECT polygonid, layerid, name, isvisible, color, opacity FROM polygon WHERE layerid = '"
+							+ layer.getObjectId() + "';");
+
+			while (resultSet.next()) {
+
+				IGeometry polygon = new Polygon(resultSet.getLong("polygonid"));
+
 				polygon.setParent(layer);
 				polygon.setName(resultSet.getString("name"));
 				polygon.setVisibility(resultSet.getBoolean("isvisible"));
+				System.out.println("Color on Polygon: "+resultSet.getInt("color"));
 				polygon.setColor(new Color(resultSet.getInt("color")));
 				polygon.setOpacity(resultSet.getFloat("opacity"));
 
 				PPoint pPoint = new PPoint();
-				//pPoint.				
-				
-				if(polygon instanceof Polygon){
+				// pPoint.
+
+				if (polygon instanceof Polygon) {
 					((Polygon) polygon).getPoints().clear();
-					((Polygon) polygon).getPoints().addAll(pPoint.loadFromDB(polygon));
+					((Polygon) polygon).getPoints().addAll(
+							pPoint.loadFromDB(polygon));
 				}
-				
-				
+
 				polygonList.add(polygon);
 			}
-			
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		dataManager.dispose();
 
 		return polygonList;
 	}
-	
+
 }
