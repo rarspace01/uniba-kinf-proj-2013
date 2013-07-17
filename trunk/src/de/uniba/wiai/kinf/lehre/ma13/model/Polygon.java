f6c3456a29a31b93662403798df7ad3540fb5b3d
denishamann@gmail.com <denishamann@gmail.com@e6a66caa-e9e3-d0a2-5608-3df70dc90c32>
2013-07-17 20:13:29 +0000

    *DOC

--- trunk/src/de/uniba/wiai/kinf/lehre/ma13/model/Polygon.java
+++ trunk/src/de/uniba/wiai/kinf/lehre/ma13/model/Polygon.java
@@ -9,19 +9,19 @@ import de.uniba.wiai.kinf.lehre.ma13.model.interfaces.IGeometry;
 import de.uniba.wiai.kinf.lehre.ma13.model.interfaces.ILayer;
 
 /**
+ * polygon class. handles the polygon and its containing {@link Point}s
  * 
  * @author denis
- *
+ * 
  */
 public class Polygon extends OrderedObject implements IGeometry {
 
 	private ILayer parent_;
-	
+
 	/** list of points, position in "units" in respect to the background image */
 	private List<Point> points_;
-	
-	public Polygon(Long objectId)
-	{
+
+	public Polygon(Long objectId) {
 		setObjectId(objectId);
 		points_ = new LinkedList<Point>();
 	}
@@ -35,24 +35,28 @@ public class Polygon extends OrderedObject implements IGeometry {
 	public void setParent(ILayer parentLayer) {
 		parent_ = parentLayer;
 	}
-	
-	public List<Point> getPoints()
-	{
+
+	/**
+	 * retrieves the {@link List} of {@link Point}s in the {@link Polygon}
+	 * 
+	 * @return
+	 */
+	public List<Point> getPoints() {
 		return points_;
 	}
 
 	@Override
 	public boolean inBoundingBox(int x1, int x2, int y1, int y2) {
 		// create bounding box
-		Rectangle boundingBox = new Rectangle(x1, y1, Math.abs(x1-x2), Math.abs(y1-y2));
-		
+		Rectangle boundingBox = new Rectangle(x1, y1, Math.abs(x1 - x2),
+				Math.abs(y1 - y2));
+
 		// create polygon
 		java.awt.Polygon polygon = new java.awt.Polygon();
-		for(Point point : points_)
-		{
+		for (Point point : points_) {
 			polygon.addPoint(point.x, point.y);
 		}
-		
+
 		// check if bounding box of polygon intersects with given box
 		return polygon.getBounds().intersects(boundingBox);
 	}
